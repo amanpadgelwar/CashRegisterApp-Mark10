2b483c174101a9f81ebd9ccc79ef64b5bcca3c13
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2022-12-24 19:27:42 +0100

    ADDED JS

--- index.js
+++ index.js
@@ -2,9 +2,47 @@ const billAmount = document.querySelector("#bill-amount");
 const cashAmount = document.querySelector("#cash-amount");
 var checkButton =document.querySelector("#check-button");
 var error = document.querySelector("#error-message");
-console.log(cashAmount.value);
-console.log(billAmount.value);
+var noofNotes =document.querySelectorAll("")
+var availableNotes=[2000,500,100,20,10,5,1];
 
-function checkBillAndCashAmount(){console.log("clicked")};
+function checkBillAndCashAmount()
+{
+hideMessage();
+    if(billAmount.value>0){
+        if(cashAmount.value > billAmount.value){
+            var returnAmount= cashAmount.value-billAmount.value;
+    }
+        else{
+            messageHandle("Cash amount should be greater than or equal to the  bill amount")
+        }
+
+    }else{
+        messageHandle("Invalid Bill Amount")
+    }
+
+
+function calculateChange(returnAmount){
+    for (let i=0;i<availableNotes.length;i++){
+     var amoutofNotes=Math.trunc(
+        returnAmount/availableNotes[0]
+     );
+
+     returnAmount%=availableNotes[0]
+
+    }
+
+}
+
+function hideMessage(){
+    error.style.display ="none";  
+}
+
+ function messageHandle(message){
+    error.style.display="block";
+    error.innertext = message;
+ }
+
+
+};
 checkButton.addEventListener("click", checkBillAndCashAmount());
 
