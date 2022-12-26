db3df4de94f815ffcaa90bdacc597679b746d001
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2022-12-26 14:04:56 +0100

    styled body

--- index.js
+++ index.js
@@ -1,50 +1,70 @@
-var billAmount = document.getElementById('bill-amount');
-var cashAmount = document.getElementById('cash-amount');
-var checkButton =document.getElementById('check-button');
+var billAmount = document.getElementById('billamount');
+var cashAmount = document.getElementById('cashamount');
+var nextButton =document.getElementById('nextbutton');
+var checkButton =document.getElementById('checkbutton');
 var error = document.getElementById('message');
-var noofNotes =document.getElementsByClassName('no-Of-Notes');
+var noofNotes =document.querySelectorAll(".no-Of-Notes");
 var availableNotes=[2000,500,100,20,10,5,1];
-console.log("clicked");
-
-checkButton.addEventListener("click",function checkBillAndCashAmount()
-{
-hideMessage();
-    if(billAmount.value>0){
-        if(cashAmount.value > billAmount.value){
-            var returnAmount= cashAmount.value-billAmount.value;
-    }
-        else{
-            messageHandle("Cash amount should be greater than or equal to the  bill amount")
-        }
-
-    }else{
-        messageHandle("Invalid Bill Amount")
-    }
-
-
-function calculateChange(returnAmount){
-    for (let i=0;i<availableNotes.length;i++){
-     var amoutofNotes=Math.trunc(
-        returnAmount/availableNotes[i]
-     );
-
-     returnAmount%=availableNotes[i];
-     noofNotes[i].innerHTML =amoutofNotes;
-
-    }
-
-}
+const returnAmount= cashAmount.value-billAmount.value;
 
 function hideMessage(){
     error.style.display ="none";  
 }
 
- function messageHandle(message){
-    error.style.display="block";
-    error.innertext = message;
+function messageHandle(msg){
+    error.style.display="block"
+    error.innerHTML = msg;
  }
 
+ 
+checkButton.addEventListener("click",() =>
+{
+    let bill =Number(billAmount.value)
+    let cash =Number(cashAmount.value)
+    hideMessage();
 
-});
+    if(bill> 0){
+        if(cash > bill){
+            const returnAmount= cash -bill;
+            calculateChange(returnAmount);
+            console.log(returnAmount)
+            
+    }
+        else{ 
+            messageHandle("Cash amount should be greater than or equal to the  bill amount")
+        }
 
+    }
+    
+    else
+    {
+        messageHandle("Invalid Bill Amount")
+    }
+
+})
+
+
+ 
+
+ 
+
+
+
+
+
+
+
+function calculateChange(returnAmount){
+    for (let i=0;i <=8;i++){
+     var amoutofNotes=Math.trunc(
+        returnAmount/availableNotes[i]
+     );
+     
+
+     returnAmount%=availableNotes[i];
+     noofNotes[i].innerHTML =amoutofNotes;
+     
+    }
+
+}
 
